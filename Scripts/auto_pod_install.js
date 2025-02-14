107d02521bd87a1df4e6a5574d7d516b539144ed
ravidas.r <ravidas.r@sankalp.com>
2025-02-14 12:53:20 +0530

    pod1

---
+++ Scripts/auto_pod_install.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const exec = require('child_process').execSync;
+
+module.exports = function(context) {
+    const iosPlatformPath = path.join(context.opts.projectRoot, 'platforms', 'ios');
+
+    if (fs.existsSync(iosPlatformPath)) {
+        console.log('Generating Podfile...');
+        exec('cordova platform add ios --nosave', { stdio: 'inherit' });
+
+        console.log('Running pod install...');
+        exec('pod install', { cwd: iosPlatformPath, stdio: 'inherit' });
+    } else {
+        console.log('iOS platform not found. Skipping pod install.');
+    }
+};
